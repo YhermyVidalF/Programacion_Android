d10f47106409c727becbf933562931e5f88683f7
YhermyVidalF <90571748+YhermyVidalF@users.noreply.github.com>
2021-09-12 23:55:23 -0300

    2.0

--- Contador/app/src/main/java/com/example/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/contador/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int nClics=0;
+    private TextView txtShowClics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtShowClics =(TextView) findViewById(R.id.txt_show_view);
+        txtShowClics.setText(Integer.toString(nClics));
+        }
+
+        public void mostrarTostada(View view){
+            Toast.makeText(this,"Demostración de una tostada", Toast.LENGTH_SHORT).show();
+        }
+
+        public void contarClics(View view){
+             nClics++;
+             txtShowClics.setText(Integer.toString(nClics));
+        }
+
+        public void menosClics( View view){
+              nClics--;
+              txtShowClics.setText(Integer.toString(nClics));
+        }
+        public void resetClics(View view){
+              nClics=0;
+              txtShowClics.setText(Integer.toString(nClics));
+        }
     }
-}
